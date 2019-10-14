e25c5a17856900dce12c22a5035b1303ba0c334c
MuchToKnow <justindc99edu@gmail.com>
2019-10-13 20:29:55 -0700

    Cleanup and comments

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Private project repo for CPSC 312
 Install deps:
 `cabal update && cabal install hip`
 
-To Run:
+To Run without building:
+`./Visualizer`
+
+To Build and Run (Do this if you have made changes to the source Visualizer.hs):
 `ghc Visualizer.hs && ./Visualizer`
--- src/Visualizer.hs
+++ src/Visualizer.hs
@@ -2,18 +2,27 @@ import Prelude as P
 import Graphics.Image as I
 import Data.Complex as C
 import Data.Time.Clock.POSIX
+
 main = do
-    x <- getInt "Input an integral x size"
-    y <- getInt "Input an integral y size"
-    itr <- getInt "How many iterations when creating Julia Set"
+    x <- getInt "Input an integral width"
+    y <- getInt "Input an integral height"
+    itr <- getInt "Input an integral number of iterations to update f(z) = z^2 + c"
     time <- round `fmap` getPOSIXTime
     let c = (-0.7269) :+ (0.1889) :: Complex Double
+    -- jSet is the image array produced by outputting black (0) if the pixel is in the set and white (1) otherwise
     let jSet = makeImageR RPU (y, x) (\(i, j) -> PixelY $ if inSet (normalizer (x,y) (i,j)) c itr then 0 else 1) :: Image RPU Y Double
     writeImage ("../out/" ++ (show time) ++ ".png") jSet
 
--- Normalizes the pixel (i,j) (i-th row, j-th column) from -2 to 2 given the dimension of the output (x,y)
-normalizer (x,y) (i,j) = (fromIntegral (y-(fromIntegral i)*2)/(fromIntegral y), fromIntegral (x-(fromIntegral j)*2)/(fromIntegral x))
+-- Normalizes the pixel (i,j) (i-th row, j-th column) from -2 to 2 given the total dimension of the output (x,y) where there are
+-- x rows and y columns
+normalizer :: (Int, Int) -> (Int, Int) -> (Double, Double)
+normalizer (x,y) (i,j) = do
+    -- Double versions of x and y for division
+    let dx = fromIntegral x :: Double
+    let dy = fromIntegral y :: Double
+    (fromIntegral (y-(i*2)) / dy, fromIntegral (x-(j*2)) / dx)
 
+-- Reads an Int from standard in
 getInt :: (Read b, Integral b) => String -> IO b
 getInt s = do
     putStrLn s

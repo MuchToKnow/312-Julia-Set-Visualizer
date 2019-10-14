e8a15f32133852108bed5b9bf8797ac95dc9f700
MuchToKnow <justindc99edu@gmail.com>
2019-10-13 20:11:22 -0700

    Normalized pixels from -2 to 2 to center image produced

--- src/Visualizer.hs
+++ src/Visualizer.hs
@@ -7,9 +7,12 @@ main = do
     y <- getInt "Input an integral y size"
     itr <- getInt "How many iterations when creating Julia Set"
     time <- round `fmap` getPOSIXTime
-    let c = (0.3) :+ (0.01) :: Complex Double
-    let jSet = makeImageR RPU (x, y) (\(i, j) -> PixelY $ if inSet ((fromIntegral i)*2/(fromIntegral x), (fromIntegral j)*2/(fromIntegral y)) c itr then 0 else 1) :: Image RPU Y Double
+    let c = (-0.7269) :+ (0.1889) :: Complex Double
+    let jSet = makeImageR RPU (y, x) (\(i, j) -> PixelY $ if inSet (normalizer (x,y) (i,j)) c itr then 0 else 1) :: Image RPU Y Double
     writeImage ("../out/" ++ (show time) ++ ".png") jSet
+
+-- Normalizes the pixel (i,j) (i-th row, j-th column) from -2 to 2 given the dimension of the output (x,y)
+normalizer (x,y) (i,j) = (fromIntegral (y-(fromIntegral i)*2)/(fromIntegral y), fromIntegral (x-(fromIntegral j)*2)/(fromIntegral x))
 
 getInt :: (Read b, Integral b) => String -> IO b
 getInt s = do

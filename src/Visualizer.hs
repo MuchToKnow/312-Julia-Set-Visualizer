3e1a1e0e9adfbd4b33e5de5cb9d99643baeb3e02
MuchToKnow <justindc99edu@gmail.com>
2019-10-13 17:10:38 -0700

    First try Julia Set Visualizer

---
+++ src/Visualizer.hs
@@ -0,0 +1,25 @@
+import Prelude as P
+import Graphics.Image as I
+import Data.Complex as C
+main = do
+    x <- getInt "Input an integral x size"
+    y <- getInt "Input an integral y size"
+    itr <- getInt "How many iterations when creating Julia Set"
+    let c = (0.3) :+ (0.01) :: Complex Double
+    let jSet = makeImageR RPU (x, y) (\(i, j) -> PixelY $ if inSet (i,j) c itr then 0 else 1) :: Image RPU Y Double
+    writeImage "j.png" jSet
+
+getInt :: (Read b, Integral b) => String -> IO b
+getInt s = do
+    putStrLn s
+    i <- getLine
+    return (read i)
+
+-- Returns True if given pixel (x,y) is in the Julia set, false otherwise
+inSet :: (Int,Int) -> Complex Double -> Int -> Bool
+inSet (x,y) c itr = C.magnitude(juliaIterate (fromIntegral x :+ fromIntegral y) c itr) < 2
+
+-- Updates the complex number Z itr times according to z = z^2 + c
+juliaIterate :: Complex Double -> Complex Double -> Int -> Complex Double
+juliaIterate z c 0 = z
+juliaIterate z c itr = juliaIterate (z^2 + c) c (itr-1)
